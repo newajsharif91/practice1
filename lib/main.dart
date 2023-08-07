9752155fbb930520b04e4798e079cd30f5a7a0d3
“[newajsharif91]” <[email>
2023-08-07 21:48:09 +0600

    Hello word run with different way

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+ main(){
+   runApp(const MyApp());//Aplication
+ }
+ class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
-main (){
-  runApp(app)//Application
-}
+  @override
+  Widget build(BuildContext context) {
+   return const MaterialApp(home:HomeActivity());
+  }
+
+ }
+ class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    appBar:AppBar(title: Text("First App"),),
+    body: Text("Hello word"),
+   );
+
+  }
+
+ }
