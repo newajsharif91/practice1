233515c98be587029e62488fc94bd508d1836b05
“[newajsharif91]” <[email>
2023-08-19 23:27:32 +0600

    calculator app create

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,110 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
- main(){
-   runApp(const MyApp());//Aplication
- }
- class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My First Calculator')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              'Input field', // Display the ongoing calculation or result here
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              ),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ,),
+              textAlign: TextAlign.end,
+              readOnly: true,
+              controller: TextEditingController(text: 'give me input'), // Replace '579' with your current input
+            )
+            ,
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalculatorButton(text: '7'),
+                CalculatorButton(text: '8'),
+                CalculatorButton(text: '9'),
+                CalculatorButton(text: '÷'),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalculatorButton(text: '4'),
+                CalculatorButton(text: '5'),
+                CalculatorButton(text: '6'),
+                CalculatorButton(text: '×'),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalculatorButton(text: '1'),
+                CalculatorButton(text: '2'),
+                CalculatorButton(text: '3'),
+                CalculatorButton(text: '-'),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CalculatorButton(text: '0'),
+                CalculatorButton(text: '.'),
+                CalculatorButton(text: '='),
+                CalculatorButton(text: '+'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CalculatorButton extends StatelessWidget {
+  final String text;
+
+  const CalculatorButton({required this.text});
 
   @override
   Widget build(BuildContext context) {
-   return const MaterialApp(home:HomeActivity());
+    return Container(
+      width: 80,
+      height: 80,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
-
- }
- class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-    appBar:AppBar(title: Text("First App"),),
-    body: Text("Hello word"),
-   );
-
-  }
-
- }
+}
